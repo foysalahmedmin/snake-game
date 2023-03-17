dd830a2e734c52e8806139b1a9a049431d96955f
FOYSAL AHMED MiN <foysalahmedmin@gmail.com>
2023-03-18 02:59:50 +0600

    food positioning

--- script/app.js
+++ script/app.js
@@ -12,6 +12,10 @@ let snakeY = block_size * 5 ;
 let move_directionX = 0 ;
 let move_directionY = 0 ;
 
+// food;
+let foodX;
+let foodY;
+
 window.onload = () => {
     board = document.getElementById("game-board");
     board.height = rows * block_size ;
@@ -25,9 +29,23 @@ window.onload = () => {
 }
 
 const update = () => {
-    boardCanvas.fillStyle = "black"
+    boardCanvas.fillStyle = "black";
     boardCanvas.fillRect(0, 0, board.width, board.height);
 
+    foodPosition();
+    boardCanvas.fillStyle = "red";
+    boardCanvas.fillRect(foodX, foodY, block_size, block_size);
+
     boardCanvas.fillStyle = "lime";
+    snakeX += move_directionX * block_size ;
+    snakeY += move_directionY * block_size ;
     boardCanvas.fillRect(snakeX , snakeY , block_size, block_size);
+
+    
+}
+
+const foodPosition = () => {
+    console.log(board.height, board.width);
+    foodX = board.height/ 2 ;
+    foodY = board.width /2 ;
 }
