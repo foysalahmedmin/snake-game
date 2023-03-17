c3c3253375c68946b0938e78443fe784ca9da2c6
FOYSAL AHMED MiN <foysalahmedmin@gmail.com>
2023-03-18 02:20:34 +0600

    snake game board desing

---
+++ script/app.js
@@ -0,0 +1,25 @@
+// Board;
+const block_size = 25 ;
+const rows = 20 ;
+const cols = 20 ;
+let board;
+let boardCanvas;
+
+// snake;
+let snakeX = block_size * 5 ;
+let snakeY = block_size * 5 ;
+
+let move_directionX = 0 ;
+let move_directionY = 0 ;
+
+window.onload = () => {
+    board = document.getElementById("game-board");
+    board.height = rows * block_size ;
+    board.width = cols * block_size ;
+    boardCanvas = board.getContext("2d");
+    console.log(board , boardCanvas);
+
+    boardCanvas.fillStyle = "black"
+    boardCanvas.fillRect(0, 0, board.width, board.height);
+
+}
