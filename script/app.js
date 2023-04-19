4fc2f46c3d7c53126920f30f058f5e90837af2c1
FOYSAL AHMED MiN <foysalahmedmin@gmail.com>
2023-04-20 02:18:44 +0600

    game resposive functionality complete

--- script/app.js
+++ script/app.js
@@ -1,16 +1,27 @@
+// Controller Buttons;
+const controllers = document.getElementById('controllers')
+const up = document.getElementById('up')
+const down = document.getElementById('down')
+const left = document.getElementById('left')
+const right = document.getElementById('up')
+
+// Block Size;
+const blockSizeDiv = document.getElementById('blockSize');
+const blockSize = window.getComputedStyle(blockSizeDiv).getPropertyValue("font-size").slice(0, -2);
+
 // Board;
-const block_size = 25 ;
-const rows = 20 ;
-const cols = 20 ;
+const block_size = +blockSize;
+const rows = 20;
+const cols = 20;
 let board;
 let boardCanvas;
 
 // snake;
-let snakeX = block_size * 5 ;
-let snakeY = block_size * 5 ;
+let snakeX = block_size * 5;
+let snakeY = block_size * 5;
 
-let move_directionX = 0 ;
-let move_directionY = 0 ;
+let move_directionX = 0;
+let move_directionY = 0;
 
 const snake_body = [];
 
@@ -19,24 +30,25 @@ let foodX;
 let foodY;
 
 // game over;
-let game_over = false ;
+let game_over = false;
 
 window.onload = () => {
     board = document.getElementById("game-board");
-    board.height = rows * block_size ;
-    board.width = cols * block_size ;
+    board.height = rows * block_size;
+    board.width = cols * block_size;
     boardCanvas = board.getContext("2d");
 
-    document.onkeyup = changeDirection ;
+    document.onkeyup = changeDirection;
 
     foodPosition();
     // update();
-    let updating = setInterval(() => {update()}, 100)
+    let updating = setInterval(() => { update() }, 100)
 
 }
 
 const update = () => {
-    if(game_over){
+    // stop updating ;
+    if (game_over) {
         return false;
     }
 
@@ -46,59 +58,82 @@ const update = () => {
     boardCanvas.fillStyle = "red";
     boardCanvas.fillRect(foodX, foodY, block_size, block_size);
 
-    if(snakeX == foodX & snakeY == foodY){
+    if (snakeX == foodX & snakeY == foodY) {
         snake_body.push([foodX, foodY]);
         foodPosition();
     }
 
-    for(let i = snake_body.length - 1 ; i > 0; i --){
-        snake_body[i] = snake_body[i-1];
+    for (let i = snake_body.length - 1; i > 0; i--) {
+        snake_body[i] = snake_body[i - 1];
     }
 
-    if(snake_body.length){
+    if (snake_body.length) {
         snake_body[0] = [snakeX, snakeY]
     }
 
     boardCanvas.fillStyle = "lime";
-    snakeX += move_directionX * block_size ;
-    snakeY += move_directionY * block_size ;
-    boardCanvas.fillRect(snakeX , snakeY , block_size, block_size);
+    snakeX += move_directionX * block_size;
+    snakeY += move_directionY * block_size;
+    boardCanvas.fillRect(snakeX, snakeY, block_size, block_size);
 
-    for(let i = 0; i < snake_body.length; i++){
+    for (let i = 0; i < snake_body.length; i++) {
         boardCanvas.fillRect(snake_body[i][0], snake_body[i][1], block_size, block_size);
     }
 
     // game over condition;
-    if(snakeX < 0 || snakeX > rows * block_size || snakeY < 0 || snakeY > cols * block_size){
-        game_over = true ;
+    if (snakeX < 0 || snakeX > rows * block_size || snakeY < 0 || snakeY > cols * block_size) {
+        // game over for cross the area;
+        game_over = true;
         alert("Game Over");
     }
-    for(let i = 0; i < snake_body.length; i ++){
-        if(snakeX == snake_body[i][0] && snakeY == snake_body[i][1]){
-            game_over = true ;
+    for (let i = 0; i < snake_body.length; i++) {
+        // game over for eat self ;
+        if (snakeX == snake_body[i][0] && snakeY == snake_body[i][1]) {
+            game_over = true;
             alert("Game Over");
         }
     }
 
 }
 
+// food positioning;
 const foodPosition = () => {
-    foodX = Math.floor(Math.random() * cols) * block_size ;
-    foodY = Math.floor(Math.random() * rows) * block_size ;
+    foodX = Math.floor(Math.random() * cols) * block_size;
+    foodY = Math.floor(Math.random() * rows) * block_size;
 }
 
+// snake direction;
 const changeDirection = (e) => {
-    if(e.keyCode == "38" && move_directionY != 1){
-        move_directionX = 0 ;
-        move_directionY = -1 ;
-    }else if(e.keyCode == "39" && move_directionX != -1){
-        move_directionX = 1 ;
-        move_directionY = 0 ;
-    }else if(e.keyCode == "40" && move_directionY != -1){
-        move_directionX = 0 ;
-        move_directionY = 1 ;
-    }else if(e.keyCode == "37" && move_directionX != 1){
-        move_directionX = -1 ;
-        move_directionY = 0 ;
+    if (e.keyCode == "38" && move_directionY != 1) {
+        move_directionX = 0;
+        move_directionY = -1;
+    } else if (e.keyCode == "39" && move_directionX != -1) {
+        move_directionX = 1;
+        move_directionY = 0;
+    } else if (e.keyCode == "40" && move_directionY != -1) {
+        move_directionX = 0;
+        move_directionY = 1;
+    } else if (e.keyCode == "37" && move_directionX != 1) {
+        move_directionX = -1;
+        move_directionY = 0;
+    }
+}
+
+// direction by button;
+controllers.onclick = (e) => {
+    console.log(e.target.getAttribute('id'))
+    const targetPoint = e.target.getAttribute('id')
+    if (targetPoint == "up" && move_directionY != 1) {
+        move_directionX = 0;
+        move_directionY = -1;
+    } else if (targetPoint == "right" && move_directionX != -1) {
+        move_directionX = 1;
+        move_directionY = 0;
+    } else if (targetPoint == "down" && move_directionY != -1) {
+        move_directionX = 0;
+        move_directionY = 1;
+    } else if (targetPoint == "left" && move_directionX != 1) {
+        move_directionX = -1;
+        move_directionY = 0;
     }
 }
